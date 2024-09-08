b017222ddd51e01a3890d03572cc5ad0610de11e
nqhuyyy1308 <nguyenquanghuy130803@gmail.com>
2024-09-08 14:30:53 +0700

    abc

--- App.tsx
+++ App.tsx
@@ -4,8 +4,17 @@
  *
  * @format
  */
-import React, {useEffect} from 'react';
-import {Image, Platform, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect } from 'react';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Carousel from 'react-native-snap-carousel';
+import SplashScreen from 'react-native-splash-screen'
 
 import SplashScreen from 'react-native-splash-screen';
 
@@ -13,6 +22,35 @@ const App = () => {
   useEffect(() => {
     if (Platform.OS === 'android') SplashScreen.hide();
   }, []);
+
+  const carouselItems = [
+    {
+        title:"Item 1",
+        text: "Text 1",
+    },
+    {
+        title:"Item 2",
+        text: "Text 2",
+    },
+    {
+        title:"Item 3",
+        text: "Text 3",
+    },
+    {
+        title:"Item 4",
+        text: "Text 4",
+    },
+    {
+        title:"Item 5",
+        text: "Text 5",
+    },
+    ]
+
+    const renderItem = () => {
+      return <Text>
+        Hello
+      </Text>
+    }
 
   return (
     <View style={styles.container}>
@@ -29,9 +67,16 @@ const App = () => {
 
       <View style={styles.footerSection}>
         <Text style={styles.animationText}>Skip</Text>
+        <Carousel
+              data={carouselItems}
+              renderItem={renderItem}
+              sliderWidth={300}
+              itemWidth={300}
+            />
         <Text style={styles.animationText}>Next</Text>
       </View>
     </View>
+
   );
 };
 const styles = StyleSheet.create({
