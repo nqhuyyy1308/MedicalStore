e2edffae4904ee23e7a0b5a74ae6affe7259ffaf
Anhkhoi.vuong@optimalvest.com <Anhkhoi.vuong@optimalvest.com>
2024-09-08 15:14:49 +0700

    fix bug

--- App.tsx
+++ App.tsx
@@ -4,81 +4,88 @@
  *
  * @format
  */
-import React, { useEffect } from 'react';
-import {
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Image, Platform, StyleSheet, Text, View} from 'react-native';
 
 import Carousel from 'react-native-snap-carousel';
-import SplashScreen from 'react-native-splash-screen'
-
 import SplashScreen from 'react-native-splash-screen';
 
+type OnboardingCarousel = {
+  id: string;
+  title: string;
+  descriptions: string;
+};
+
+const carouselItems: OnboardingCarousel[] = [
+  {
+    id: '01',
+    title: 'View and buy Medicine online',
+    descriptions:
+      'Etiam mollis metus non purus faucibus sollicitudin. Pellentesque sagittis mi. Integer.',
+  },
+  {
+    id: '02',
+    title: 'View 02',
+    descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
+  },
+  {
+    id: '03',
+    title: 'View 03',
+    descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
+  },
+  {
+    id: '04',
+    title: 'View 04',
+    descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
+  },
+];
+
 const App = () => {
+  const carouselRef = useRef();
+
   useEffect(() => {
     if (Platform.OS === 'android') SplashScreen.hide();
   }, []);
 
-  const carouselItems = [
-    {
-        title:"Item 1",
-        text: "Text 1",
-    },
-    {
-        title:"Item 2",
-        text: "Text 2",
-    },
-    {
-        title:"Item 3",
-        text: "Text 3",
-    },
-    {
-        title:"Item 4",
-        text: "Text 4",
-    },
-    {
-        title:"Item 5",
-        text: "Text 5",
-    },
-    ]
+  const renderItem = ({item}: {item: OnboardingCarousel}) => {
+    return (
+      <View>
+        <Text style={styles.textH1}>{item.title}</Text>
+        <Text style={styles.textDesc}>{item.descriptions}</Text>
+      </View>
+    );
+  };
 
-    const renderItem = () => {
-      return <Text>
-        Hello
-      </Text>
-    }
+  const onScrollIndexChanged = (index: number) => {
+    console.log('current index', index);
+  };
 
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.imageOnboard}
-        source={require('./src/assets/images/onboard_image.png')}></Image>
-
-      <Text style={styles.textH1}> View and buy Medicine online </Text>
-      <Text style={styles.textDesc}>
-        {' '}
-        Etiam mollis metus non purus faucibus sollicitudin. Pellentesque
-        sagittis mi. Integer.{' '}
-      </Text>
+      <View style={styles.imageWrapper}>
+        <Image
+          source={require('./src/assets/images/onboard_image.png')}
+          style={styles.imageOnboard}
+        />
+      </View>
+      <Carousel
+        ref={carouselRef}
+        data={carouselItems}
+        renderItem={renderItem}
+        sliderWidth={300}
+        itemWidth={300}
+        slideStyle={styles.carouselViewContainer}
+        onScrollIndexChanged={onScrollIndexChanged}
+      />
 
       <View style={styles.footerSection}>
         <Text style={styles.animationText}>Skip</Text>
-        <Carousel
-              data={carouselItems}
-              renderItem={renderItem}
-              sliderWidth={300}
-              itemWidth={300}
-            />
         <Text style={styles.animationText}>Next</Text>
       </View>
     </View>
-
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -86,7 +93,16 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'center',
   },
-
+  carouselViewContainer: {
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 0,
+  },
+  imageWrapper: {
+    flex: 2,
+    justifyContent: 'flex-end',
+  },
   textH1: {
     width: 238,
     color: '#090F47',
@@ -111,13 +127,10 @@ const styles = StyleSheet.create({
   imageOnboard: {
     height: 284,
     width: 256.3,
-    marginBottom: 48,
   },
 
   footerSection: {
-    display: 'flex',
     flexDirection: 'row',
-    bottom: 0,
   },
 
   animationText: {},
