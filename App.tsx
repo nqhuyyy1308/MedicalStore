72c3b7cf995c3ba73a67bacfe1b02112de65d9c9
nqhuyyy1308 <nguyenquanghuy130803@gmail.com>
2024-09-08 14:06:31 +0700

    add UI onboarding screen

--- App.tsx
+++ App.tsx
@@ -4,122 +4,78 @@
  *
  * @format
  */
-import React, { useEffect } from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect} from 'react';
+import {Image, Platform, StyleSheet, Text, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import SplashScreen from 'react-native-splash-screen';
 
-import SplashScreen from 'react-native-splash-screen'
+const App = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android') SplashScreen.hide();
+  }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View style={styles.container}>
+      <Image
+        style={styles.imageOnboard}
+        source={require('./src/assets/images/onboard_image.png')}></Image>
+
+      <Text style={styles.textH1}> View and buy Medicine online </Text>
+      <Text style={styles.textDesc}>
+        {' '}
+        Etiam mollis metus non purus faucibus sollicitudin. Pellentesque
+        sagittis mi. Integer.{' '}
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+
+      <View style={styles.footerSection}>
+        <Text style={styles.animationText}>Skip</Text>
+        <Text style={styles.animationText}>Next</Text>
+      </View>
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  useEffect(() => {
-    if(Platform.OS === 'android')
-    SplashScreen.hide();
-  }, [])
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug"> Yet sorrr
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
+};
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
   },
-  sectionTitle: {
+
+  textH1: {
+    width: 238,
+    color: '#090F47',
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
     fontWeight: '700',
+    textAlign: 'center',
+    lineHeight: 32,
+    letterSpacing: 1,
   },
+
+  textDesc: {
+    width: 255,
+    color: '#090F4773',
+    fontSize: 16,
+    fontWeight: '300',
+    textAlign: 'center',
+    lineHeight: 24,
+    letterSpacing: 0.5,
+    marginTop: 16,
+  },
+
+  imageOnboard: {
+    height: 284,
+    width: 256.3,
+    marginBottom: 48,
+  },
+
+  footerSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    bottom: 0,
+  },
+
+  animationText: {},
 });
 
 export default App;
