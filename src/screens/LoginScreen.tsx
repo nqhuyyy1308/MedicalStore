d1cde1e5887e8d5161406a80242d50d48d87c1ab
nqhuyyy1308 <nguyenquanghuy130803@gmail.com>
2024-09-09 12:15:58 +0700

    Finish Onboarding screen

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+
+
+
+const LoginScreen = () => {
+    return(
+        <View>
+            <Text>
+                Login Screen
+            </Text>
+        </View>
+    );
+}
+
+export default LoginScreen;
