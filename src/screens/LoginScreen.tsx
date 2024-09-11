7467e8c170e352bce80449f414097cece1d9a934
Nqhuyyy <nguyenquanghuy130803@gmail.com>
2024-09-11 11:34:22 +0700

    abc

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,77 +1,72 @@
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
-
-
-
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const LoginScreen = () => {
-    return(
-        <View style={styles.container}>
-            <View style={styles.logoWrapper}>
-                <Image style={styles.logoImage} source={require('../assets/images/login_logo.png')}></Image>
-                <Text style={styles.logoText}> Nilkanth Medical </Text>
-            </View>
-            <View style={styles.formWrapper}>
-                <Text style={styles.formText}> Please Enter your Mobile Number to Login/Sign Up </Text>
-                <TextInput 
-                placeholder="Your Phone"
-                
-                />
-            </View>
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoWrapper}>
+        <Image
+          style={styles.logoImage}
+          source={require('../assets/images/login_logo.png')}></Image>
+        <Text style={styles.logoText}> Nilkanth Medical </Text>
+      </View>
+      <View style={styles.formWrapper}>
+        <Text style={styles.formText}>
+          {' '}
+          Please Enter your Mobile Number to Login/Sign Up{' '}
+        </Text>
+        <TextInput placeholder="Your Phone" />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    logoWrapper: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: "center",
-    },
+  logoWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
 
-    
-    logoImage: {
-        width: 100,
-        height: 100,
-        justifyContent: "center",
-    },
-    
-    logoText: {
-        fontSize: 26,
-        color: '#000000',
-        fontStyle: "italic",
-        fontWeight: '500'
-        
-    },
-    
-    formWrapper: {   
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center",
-    },
+  logoImage: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+  },
 
-    formText: {
-        fontSize: 20,
-        fontWeight: '300',
-        color: '#090F4773',
-        lineHeight: 24,
-        textAlign: 'left'
-    },
+  logoText: {
+    fontSize: 26,
+    color: '#000000',
+    fontStyle: 'italic',
+    fontWeight: '500',
+  },
 
-    formInput: {
-        width: 300,
-        height: 50,
-        borderWidth: 1,
-        padding: 10,
-        margin: 12
-        
-    }
-    
-})
+  formWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  formText: {
+    fontSize: 20,
+    fontWeight: '300',
+    color: '#090F4773',
+    lineHeight: 24,
+    textAlign: 'left',
+  },
+
+  formInput: {
+    width: 300,
+    height: 50,
+    borderWidth: 1,
+    padding: 10,
+    margin: 12,
+  },
+});
 
 export default LoginScreen;
--- src/screens/OnboardScreen.tsx
+++ src/screens/OnboardScreen.tsx
@@ -3,71 +3,70 @@ import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 
 type OnboardingCarousel = {
-    id: string;
-    title: string;
-    descriptions: string;
-  };
-  
-const carouselItems: OnboardingCarousel[] = [
-    {
-      id: '01',
-      title: 'View and buy Medicine online',
-      descriptions:
-        'Etiam mollis metus non purus faucibus sollicitudin. Pellentesque sagittis mi. Integer.',
-    },
-    {
-      id: '02',
-      title: 'View 02',
-      descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
-    },
-    {
-      id: '03',
-      title: 'View 03',
-      descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
-    },
-    {
-      id: '04',
-      title: 'View 04',
-      descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
-    },
-  ];
+  id: string;
+  title: string;
+  descriptions: string;
+};
 
-  const OnboardScreen = ({ navigation }:any) => {
+const carouselItems: OnboardingCarousel[] = [
+  {
+    id: '01',
+    title: 'View and buy Medicine online',
+    descriptions:
+      'Etiam mollis metus non purus faucibus sollicitudin. Pellentesque sagittis mi. Integer.',
+  },
+  {
+    id: '02',
+    title: 'View 02',
+    descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
+  },
+  {
+    id: '03',
+    title: 'View 03',
+    descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
+  },
+  {
+    id: '04',
+    title: 'View 04',
+    descriptions: 'Etiam mollis metus non purus faucibus sollicitudin',
+  },
+];
+
+const OnboardScreen = ({navigation}: any) => {
   const carouselRef = useRef();
 
   const [indexSelected, setIndexSelected] = useState(0);
   const renderItem = (item: OnboardingCarousel) => {
-    return(
-    <View>
+    return (
+      <View>
         <Text style={styles.textH1}>{item.title}</Text>
         <Text style={styles.textDesc}>{item.descriptions}</Text>
-    </View>
-    )
-  }
+      </View>
+    );
+  };
 
-  const onScrollIndexChanged = (index:number) => {
-    console.log('index', index)
-    setIndexSelected(index)
-  }
+  const onScrollIndexChanged = (index: number) => {
+    console.log('index', index);
+    setIndexSelected(index);
+  };
 
   const onClickSkip = () => {
-    if(indexSelected > 0){
-      setIndexSelected(indexSelected - 1)
-      carouselRef.current.snapToItem(indexSelected - 1)
+    if (indexSelected > 0) {
+      setIndexSelected(indexSelected - 1);
+      carouselRef.current.snapToItem(indexSelected - 1);
     }
-  }
+  };
 
   const onClickNext = () => {
-    if(indexSelected < carouselItems.length - 1){
-      setIndexSelected(indexSelected + 1)
-      carouselRef.current.snapToItem(indexSelected + 1)
+    if (indexSelected < carouselItems.length - 1) {
+      setIndexSelected(indexSelected + 1);
+      carouselRef.current.snapToItem(indexSelected + 1);
     }
 
-    if(indexSelected === carouselItems.length - 1){
-        navigation.navigate('Login')
+    if (indexSelected === carouselItems.length - 1) {
+      navigation.navigate('Login');
     }
-  }
-
+  };
 
   return (
     <View style={styles.container}>
@@ -78,134 +77,163 @@ const carouselItems: OnboardingCarousel[] = [
         />
       </View>
       <Carousel
-              ref={carouselRef}
-              data={carouselItems}
-              renderItem={({item}) => renderItem(item)}
-              sliderWidth={300}
-              itemWidth={300}
-              slideStyle = {styles.carouselViewContainer}
-              onScrollIndexChanged = {onScrollIndexChanged}
-            />
+        ref={carouselRef}
+        data={carouselItems}
+        renderItem={({item}) => renderItem(item)}
+        sliderWidth={300}
+        itemWidth={300}
+        slideStyle={styles.carouselViewContainer}
+        onScrollIndexChanged={onScrollIndexChanged}
+      />
       <View style={styles.footerSection}>
-        <TouchableOpacity onPress={onClickSkip} style={styles.preAnimationClick}><Text style={[styles.preAnimationText, indexSelected > 0 && styles.isPreAnimation]}>Skip</Text></TouchableOpacity>
+        <TouchableOpacity
+          onPress={onClickSkip}
+          style={styles.preAnimationClick}>
+          <Text
+            style={[
+              styles.preAnimationText,
+              indexSelected > 0 && styles.isPreAnimation,
+            ]}>
+            Skip
+          </Text>
+        </TouchableOpacity>
         <View style={styles.wrapIndexSelect}>
-          <Text style={[styles.indexSelect, indexSelected === 0 && styles.indexSelected]}></Text>
-          <Text style={[styles.indexSelect, indexSelected === 1 && styles.indexSelected]}></Text>
-          <Text style={[styles.indexSelect, indexSelected === 2 && styles.indexSelected]}></Text>
-          <Text style={[styles.indexSelect, indexSelected === 3 && styles.indexSelected]}></Text>
+          <Text
+            style={[
+              styles.indexSelect,
+              indexSelected === 0 && styles.indexSelected,
+            ]}></Text>
+          <Text
+            style={[
+              styles.indexSelect,
+              indexSelected === 1 && styles.indexSelected,
+            ]}></Text>
+          <Text
+            style={[
+              styles.indexSelect,
+              indexSelected === 2 && styles.indexSelected,
+            ]}></Text>
+          <Text
+            style={[
+              styles.indexSelect,
+              indexSelected === 3 && styles.indexSelected,
+            ]}></Text>
         </View>
-        <TouchableOpacity onPress={onClickNext} style={styles.conAnimationClick}>
-            <Text style={styles.conAnimationText}>
-                {indexSelected === carouselItems.length - 1 ? "Login" : "Next"}
-            </Text></TouchableOpacity>
+        <TouchableOpacity
+          onPress={onClickNext}
+          style={styles.conAnimationClick}>
+          <Text style={styles.conAnimationText}>
+            {indexSelected === carouselItems.length - 1 ? 'Login' : 'Next'}
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      backgroundColor: 'white',
-      justifyContent: 'center',
-    },
-    carouselViewContainer: {
-      height: 200,
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      flex: 0,
-    },
-    imageWrapper: {
-      flex: 2,
-      justifyContent: 'flex-end',
-    },
-    textH1: {
-      width: 238,
-      color: '#090F47',
-      fontSize: 24,
-      fontWeight: '700',
-      textAlign: 'center',
-      lineHeight: 32,
-      letterSpacing: 1,
-    },
-  
-    textDesc: {
-      width: 255,
-      color: '#090F4773',
-      fontSize: 16,
-      fontWeight: '300',
-      textAlign: 'center',
-      lineHeight: 24,
-      letterSpacing: 0.5,
-      marginTop: 16,
-    },
-  
-    imageOnboard: {
-      height: 284,
-      width: 256.3,
-    },
-  
-    wrapIndexSelect: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      flexGrow: 1,
-      justifyContent: 'center',
-      marginBottom: 16
-    },
-  
-    indexSelect:{
-      height: 5,
-      width: 5,
-      borderRadius: 5,
-      backgroundColor: '#C4C4C4',
-      marginLeft: 4,
-      marginRight: 4
-    },
-  
-    indexSelected: {
-      height: 5,
-      width: 5,
-      borderRadius: 5,
-      backgroundColor: '#4157FF',
-      marginLeft: 4,
-      marginRight: 4
-    },
-  
-    footerSection: {
-      flexDirection: 'row',
-    },
-  
-    preAnimationClick: {
-      flex: 1,
-      marginBottom: 16
-    },
-  
-    preAnimationText: {
-      color: '#090F4773',
-      fontSize: 14,
-      textAlign: 'center',
-      fontWeight: '400'
-    },
-  
-    isPreAnimation: {
-      color: '#4157FF',
-      fontSize: 14,
-      textAlign: 'center',
-      fontWeight: '700'
-    },
-  
-    conAnimationClick: {
-      flex: 1,
-      marginBottom: 16
-    },
-  
-    conAnimationText: {
-      color: '#4157FF',
-      fontSize: 14,
-      textAlign: 'center',
-      fontWeight: '700'
-    },
-  });
-  
-  export default OnboardScreen;
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  carouselViewContainer: {
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 0,
+  },
+  imageWrapper: {
+    flex: 2,
+    justifyContent: 'flex-end',
+  },
+  textH1: {
+    width: 238,
+    color: '#090F47',
+    fontSize: 24,
+    fontWeight: '700',
+    textAlign: 'center',
+    lineHeight: 32,
+    letterSpacing: 1,
+  },
+
+  textDesc: {
+    width: 255,
+    color: '#090F4773',
+    fontSize: 16,
+    fontWeight: '300',
+    textAlign: 'center',
+    lineHeight: 24,
+    letterSpacing: 0.5,
+    marginTop: 16,
+  },
+
+  imageOnboard: {
+    height: 284,
+    width: 256.3,
+  },
+
+  wrapIndexSelect: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+
+  indexSelect: {
+    height: 5,
+    width: 5,
+    borderRadius: 5,
+    backgroundColor: '#C4C4C4',
+    marginLeft: 4,
+    marginRight: 4,
+  },
+
+  indexSelected: {
+    height: 5,
+    width: 5,
+    borderRadius: 5,
+    backgroundColor: '#4157FF',
+    marginLeft: 4,
+    marginRight: 4,
+  },
+
+  footerSection: {
+    flexDirection: 'row',
+  },
+
+  preAnimationClick: {
+    flex: 1,
+    marginBottom: 16,
+  },
+
+  preAnimationText: {
+    color: '#090F4773',
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '400',
+  },
+
+  isPreAnimation: {
+    color: '#4157FF',
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+
+  conAnimationClick: {
+    flex: 1,
+    marginBottom: 16,
+  },
+
+  conAnimationText: {
+    color: '#4157FF',
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+});
+
+export default OnboardScreen;
